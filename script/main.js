46a11aa642cac8a5faeeea192f20fa10d58a8cf8
rafael-arrieta <102436201+rafael-arrieta@users.noreply.github.com>
2022-06-03 01:00:52 -0300

    ya hace la primer validación

--- script/main.js
+++ script/main.js
@@ -1,43 +1,57 @@
 let btnResolver = document.getElementById('btn-resolver');
-
+let divRenderError = document.getElementById('render-error');
 
 btnResolver.addEventListener('click', ()=>{
-    
-    let datos= almacenarDatos();
-    
-    
+    datos = estructuraDatos()
+    datos = validarString(datos)
 });
 
-function almacenarDatos(){
-    
+function estructuraDatos(){
+
     let datos = {
-        ladoA: parseFloat(document.getElementById('inputA-lado').value),
-        ladoB: parseFloat(document.getElementById('inputB-lado').value),
-        ladoC: parseFloat(document.getElementById('inputC-lado').value),
+        ladoA: document.getElementById('inputA-lado').value,
+        ladoB: document.getElementById('inputB-lado').value,
+        ladoC: document.getElementById('inputC-lado').value,
         
-        gradA: parseFloat(document.getElementById('inputA-grad').value),
-        minA: parseFloat(document.getElementById('inputA-min').value),
-        segA: parseFloat(document.getElementById('inputA-seg').value),
-        degreeA:'',
-
-        gradB: parseFloat(document.getElementById('inputB-grad').value),
-        minB: parseFloat(document.getElementById('inputB-min').value),
-        segB: parseFloat(document.getElementById('inputB-seg').value),
-        degreeB:'',
-
-        gradC: parseFloat(document.getElementById('inputC-grad').value),
-        minC: parseFloat(document.getElementById('inputC-min').value),
-        segC: parseFloat(document.getElementById('inputC-seg').value),
-        degreeC:'',
+        gradA: document.getElementById('inputA-grad').value,
+        minA: document.getElementById('inputA-min').value,
+        segA: document.getElementById('inputA-seg').value,
+        //degreeA:'',
+    
+        gradB: document.getElementById('inputB-grad').value,
+        minB: document.getElementById('inputB-min').value,
+        segB: document.getElementById('inputB-seg').value,
+        //degreeB:'',
+    
+        gradC: document.getElementById('inputC-grad').value,
+        minC: document.getElementById('inputC-min').value,
+        segC: document.getElementById('inputC-seg').value,
+        //degreeC:'',
     };
-
-    datos.degreeA = aDecimal(datos.gradA,datos.minA,datos.segA);
-    datos.degreeB = aDecimal(datos.gradB,datos.minB,datos.segB);
-    datos.degreeC = aDecimal(datos.gradC,datos.minC,datos.segC);
+    
+    // datos.degreeA = aDecimal(datos.gradA,datos.minA,datos.segA);
+    // datos.degreeB = aDecimal(datos.gradB,datos.minB,datos.segB);
+    // datos.degreeC = aDecimal(datos.gradC,datos.minC,datos.segC);
     return datos;
+
 };
 
 function aDecimal(grados, minutos, segundos){
     return parseFloat((grados+(minutos/60)+(segundos/3600)).toFixed(3));
 };
 
+function validarString(obj){
+    for (const property in obj) {
+        if (obj[property].length === 0) {
+            obj[property] = 0
+        }else if(isNaN(obj[property])) {
+            obj[property]=NaN
+            divRenderError.innerHTML=`
+            hay un error o faltan datos
+            ` 
+        }else{
+            obj[property] = parseFloat(obj[property])
+        }
+        console.log(obj[property])
+    }
+}
