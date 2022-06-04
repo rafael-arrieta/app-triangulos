d8c95ee86d661143d1e51e043e0d7b70484ca81e
rafael-arrieta <102436201+rafael-arrieta@users.noreply.github.com>
2022-06-04 19:22:45 -0300

    ouvh

--- script/fnRender1.js
+++ script/fnRender1.js
@@ -2,12 +2,15 @@
 function renderBlock012(obj){
     console.log(obj)
     let block012=`
+
     $$\\cos${obj.ladoA}$$
+
     $$\\frac{1}{\\sqrt{x^2 + 1}}$$
     
     `
     
 
-    MathJax.typesetPromise()
+    MathJax.typeset()
+
     return block012
 }
